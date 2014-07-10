2e4d139c761ec3b3b4a140a4f3df19ac87c956c4
Nicholas De Souza <me@example.com>
2014-07-10 02:47:00 -0400

    Initial commit

---
+++ src/main/java/com/bazodee/jenovaproject/JenovaProject.java
@@ -0,0 +1,31 @@
+package com.bazodee.jenovaproject;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="jenovaproject", name="Jenova Project", version="1.7.10-0.1")
+public class JenovaProject
+{
+    @Mod.Instance("JenovaProject")
+    public static JenovaProject instance;
+
+    @Mod.EventHandler
+    public void preInitialization(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void initialization(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInitialization(FMLPostInitializationEvent event)
+    {
+
+    }
+}
