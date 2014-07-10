7c43567017454fbcf40aae66b1cf72ab99094cb7
Nicholas De Souza <me@example.com>
2014-07-10 15:23:21 -0400

    Added Generic block

---
+++ src/main/java/com/bazodee/jenovaproject/block/BlockJP.java
@@ -0,0 +1,40 @@
+package com.bazodee.jenovaproject.block;
+
+import com.bazodee.jenovaproject.reference.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
+
+public class BlockJP extends Block
+{
+    public BlockJP(Material material)
+    {
+        super(material);
+    }
+
+    public BlockJP()
+    {
+        super(Material.rock);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("tile.%s%s", Reference.MOD_ID + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+}
