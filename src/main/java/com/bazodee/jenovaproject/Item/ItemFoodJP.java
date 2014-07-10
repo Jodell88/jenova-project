73a46cbef22f6d9bb09b588f5ba70a713f53b24e
Nicholas De Souza <me@example.com>
2014-07-10 18:47:04 -0400

    Added Generic ItemFood class

---
+++ src/main/java/com/bazodee/jenovaproject/Item/ItemFoodJP.java
@@ -0,0 +1,11 @@
+package com.bazodee.jenovaproject.Item;
+
+import net.minecraft.item.ItemFood;
+
+public class ItemFoodJP extends ItemFood
+{
+    public ItemFoodJP(int healAmount, float saturation, boolean wolfTreat)
+    {
+        super(healAmount, saturation, wolfTreat);
+    }
+}
