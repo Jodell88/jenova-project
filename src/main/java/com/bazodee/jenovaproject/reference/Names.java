3aea67811759d04ebee498133985600b6fae6d5e
Nicholas De Souza <me@example.com>
2014-07-11 18:04:41 -0400

    Added Names reference class

---
+++ src/main/java/com/bazodee/jenovaproject/reference/Names.java
@@ -0,0 +1,41 @@
+package com.bazodee.jenovaproject.reference;
+
+public class Names
+{
+    public static class Block
+    {
+        public static final String COPPER_ORE = "copperOre";
+        public static final String TIN_ORE = "tinOre";
+        public static final String CHROMITE_ORE = "chromiteOre";
+    }
+
+    public static class Armor
+    {
+        public static final String BRONZE_ARMOR = "bronzeArmorMaterial";
+        public static final String BRONZE_HELM = "bronzeHelm";
+        public static final String BRONZE_CHEST = "bronzeChest";
+        public static final String BRONZE_LEGS = "bronzeLegs";
+        public static final String BRONZE_BOOTS = "bronzeBoots";
+    }
+
+    public static class ToolsAndWeapons
+    {
+        public static final String BRONZE_TOOL = "bronzeToolMaterial";
+        public static final String BRONZE_AXE = "bronzeAxe";
+        public static final String BRONZE_PICKAXE = "bronzePickaxe";
+        public static final String BRONZE_SHOVEL = "bronzeShovel";
+        public static final String BRONZE_SWORD = "bronzeSword";
+    }
+
+    public static class Item
+    {
+        public static final String INGOT_COPPER = "ingotCopper";
+        public static final String INGOT_TIN = "ingotTin";
+        public static final String INGOT_CHROMIUM = "ingotChromium";
+
+        public static final String RAWHIDE = "rawhide";
+
+        public static final String COOKED_EGG = "cookedEgg";
+    }
+
+}
